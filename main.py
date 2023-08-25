1efc39cbd160b8a17a703f2aaccd608cf0e3369f
Josue Toribio <josue.toribio@utec.edu.pe>
2023-08-25 02:15:51 -0500

    agregue get_by_id

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from flask import Flask, jsonify
 import json
 app = Flask(__name__)
-
-@app.route("/anime")
-def get_all_anime():
+def leer_datos():
     with open("anilist.json", "r") as archivo_json:
         datos = json.load(archivo_json)
-    return jsonify(datos)
+    return datos
+
+@app.route("/anime", methods = ['GET'])
+def get_all_anime():
+    return leer_datos()
+
+@app.route("/anime/<int:id>", methods = ['GET'])
+def get_id_anime(id):
+    datos = leer_datos()
+    get_by_id= None
+    for i in datos:
+        if i["id"]==id:
+            get_by_id = i
+            break
+    if get_by_id:
+        return get_by_id
+    else:
+        return "no se encontro el objeto"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
